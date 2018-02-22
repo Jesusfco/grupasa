f21e68f64c1e73d9bebbeeb7a6f236efedd7167e
Jesús Fco Cortés <jfcr@live.com>
2018-02-21 18:23:01 -0600

    Inicio de pagina Grupasa

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0">
+	<title>Grupasa</title>
+
+    <link href="styles/style.css" type="text/css" rel="stylesheet" media="screen,projection">
+</head>
+
+    <nav>
+        <div class="nav-content">
+            <div class="menu-logo">
+                <img src="img/logo.jpg">
+            </div>
+
+            <div class="menu-links">
+                <a>Promociones</a> |
+                <a>Productos</a> |
+                <a>Equipo</a> |
+                <a>Quienes Somos</a> |
+                <a>Contacto</a>
+            </div>    
+        </div>
+    </nav>
+
+    <div class="slider">
+        <div class="slider-img">
+            <img src="img/slider1.jpg">
+            <!-- <img src="img/slider2.jpg"> -->
+        </div>
+        
+
+        <div class="indicators">
+            <div><</div>
+            <div>></div>
+        </div>
+
+        <div class="slider-circles"></div>
+
+    </div>
+
+    <div class="ubicacion">
+        <h3>Ubicación</h3>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d243.42208021792084!2d-93.11144935150887!3d16.75275389595888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x885e4e152160edb6!2sGrupasa!5e0!3m2!1ses!2smx!4v1519228009513" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <div>            
+
+<body>
+</body>
+</html>
