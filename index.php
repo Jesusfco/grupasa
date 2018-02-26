edfdc21fdc77d929c6cffd81665bb72f619186fd
Jesús Fco Cortés <jfcr@live.com>
2018-02-26 11:31:30 -0600

    Enmaquetado slider/footer/ubicacion

--- index.php
+++ index.php
@@ -32,19 +32,66 @@
         </div>
         
 
-        <div class="indicators">
-            <div><</div>
-            <div>></div>
-        </div>
+        
+            <div class="indicators ileft"><</div>
+            <div class="indicators iright">></div>
+        
 
-        <div class="slider-circles"></div>
+        <div class="slider-circles">
+            <div class="active"></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
 
     </div>
 
     <div class="ubicacion">
-        <h3>Ubicación</h3>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d243.42208021792084!2d-93.11144935150887!3d16.75275389595888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x885e4e152160edb6!2sGrupasa!5e0!3m2!1ses!2smx!4v1519228009513" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
-    <div>            
+        <h3>DONDE ENCONTRARNOS</h3>
+        <div class="subrayado"></div>
+        <div class="ubicacion-content">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d243.42208021792084!2d-93.11144935150887!3d16.75275389595888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x885e4e152160edb6!2sGrupasa!5e0!3m2!1ses!2smx!4v1519228009513" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <div class="contact-data">
+                
+                <div>
+                    <h5>UBICACIÓN</h5>
+                    <p> Av. Central Oriente No. 683 <br>
+                        Col. Centro C.P. 29000 <br>
+                        Tuxtla Gutierrez, Chiapas.
+                    </p>
+                </div>    
+                
+                <div>
+                    <h5>HORARIO</h5>
+                    <p> LUNES A VIERNES <br>
+                        9:00 - 20:00 Hrs. <br><br>
+
+                        SÁBADOS <br>
+                        9:00 - 16:00 Hrs.
+                    </p>
+                </div>
+
+                <div>
+                    <h5>TELEFONOS</h5>
+                    <p> TEL. (961) 6114818<<br>
+                        (961) 6136232
+                    </p>
+                </div>
+
+                <div>
+                    <h5>ESCRIBENOS</h5>
+                    <p> <a href="mailto:grupasa.ventas@gmail.com">grupasa.ventas@gmail.com </a> </p>
+                </div>
+            </div>
+        </div>
+        
+    </div>   
+    
+    <footer>
+        <h2>TODO EN UN SOLO</h2>
+        <h1>LUGAR<h1>
+        <p>Aviso de privacidad</p>
+    </footer>
 
 <body>
 </body>
