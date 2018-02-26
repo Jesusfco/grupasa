ee83da24546bae3bddecbc3b07617b8cea1c347d
Jesus Fco Cortes <jfcr@live.com>
2018-02-26 10:51:39 -0600

    small changes

--- index.php
+++ index.php
@@ -19,57 +19,19 @@
             </div>
 
             <div class="menu-links">
-                <a>Promociones</a> |
-                <a>Productos</a> |
-                <a>Equipo</a> |
-                <a>Quienes Somos</a> |
+                <a>Promociones</a> <span>|</span>
+                <a onclick="smoothScroll('productos')">Productos</a>  <span>|</span>
+                <a onclick="smoothScroll('quienes')">Equipo</a> <span>|</span>
+                <a onclick="smoothScroll('quienes')">Quienes Somos</a> <span>|</span>
                 <a onclick="smoothScroll('ubicacion')">Ubicacion</a>
             </div>    
         </div>
     </nav>
 
-    <div class="slider">
-        <div class="slider-img">
-
-            <div id="slider-piece">
-                
-                <div class="img-slider-cover" style=" background-image: url(img/slider/1.jpg)"></div>                
-                <div>
-                    Aqui hay algo
-                </div>    
-                
-            </div>    
-
-            <div id="slider-piece">
-                
-                <div class="img" style=" background-image: url(img/slider/2.jpg)"></div> 
-            </div>                
-            <div id="slider-piece">
-                <div class="img" style=" background-image: url(img/slider/3.jpg)"></div> 
-            </div>     
-            <div id="slider-piece">
-                <div class="img" style=" background-image: url(img/slider/4.jpg)"></div> 
-            </div>                 
-        </div>
-        
-
-        
-        <div onclick="sliderBefore()" class="indicators ileft"><i class="material-icons">chevron_left</i></div>
-        <div onclick="sliderNext()" class="indicators iright"><i class="material-icons">chevron_right</i></div>
-        
-
-        <div class="slider-circles">            
-            
-        </div>
-
-        <footer>
-            <h2>TODO EN UN SOLO</h2>
-            <h1>LUGAR<h1>
-            <p>Aviso de privacidad</p>
-        </footer>
-
-    </div>
-
+    
+    <?php 
+        include "slider.php";
+    ?>
     
 
     <div class="ubicacion" id="div-ubicacion">
---
+++ slider.php
@@ -0,0 +1,41 @@
+<div class="slider">
+        <div class="slider-img">
+
+            <div id="slider-piece">
+                
+                <div class="img-slider-cover" style=" background-image: url(img/slider/1.jpg)"></div>                
+                <div>
+                    Aqui hay algo
+                </div>    
+                
+            </div>    
+
+            <div id="slider-piece">
+                
+                <div class="img" style=" background-image: url(img/slider/2.jpg)"></div> 
+            </div>                
+            <div id="slider-piece">
+                <div class="img" style=" background-image: url(img/slider/3.jpg)"></div> 
+            </div>     
+            <div id="slider-piece">
+                <div class="img" style=" background-image: url(img/slider/4.jpg)"></div> 
+            </div>                 
+        </div>
+        
+
+        
+        <div onclick="sliderBefore()" class="indicators ileft"><i class="material-icons">chevron_left</i></div>
+        <div onclick="sliderNext()" class="indicators iright"><i class="material-icons">chevron_right</i></div>
+        
+
+        <div class="slider-circles">            
+            
+        </div>
+
+        <footer>
+            <h2>TODO EN UN SOLO</h2>
+            <h1>LUGAR<h1>
+            <p>Aviso de privacidad</p>
+        </footer>
+
+    </div>
