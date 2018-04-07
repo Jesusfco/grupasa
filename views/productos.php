6cbd3dbf140ffb8c5925211efa4b7d9e62b1d884
Jesús Fco Cortés <jfcr@live.com>
2018-04-07 03:22:11 -0500

    correction  to products css pc

--- views/productos.php
+++ views/productos.php
@@ -1,30 +1,34 @@
 <div class="ourProducts" id="div-productos">
-    <div class="title">
-        <h3>NUESTROS PRODUCTOS</h3>
-        <div class="subrayado"></div>
-    </div>    
+    
 
     <div class="products-content">
-        <div class="productDiv s4" onclick="window.location.replace('productos/marcos.php')">
-            <img src="img/menu/marco.jpg">
-            <div id="marcos">MARCOS</div>
-        </div>
 
-        <div class="productDiv s8" onclick="window.location.replace('productos/raseros.php')">
-            <img src="img/menu/rasero.jpg">
-            <div id="raseros">RASEROS</div>
-        </div>
+        <div class="title"  width=" 100%">
+            <h3>NUESTROS PRODUCTOS</h3>
+            <div class="subrayado"></div>
+        </div>    
 
-        <div class="productDiv s4" onclick="window.location.replace('productos/papel.php')">
-            <img src="img/menu/papel.jpg">
-            <div id="papeles">PAPELES</div>
-        </div>
+        <a class="productDiv s4" href="productos/marcos.php">            
+            <div class="imgBackground" style="background-image: url(img/menu/marco.jpg)"></div>
+            <div class="square" id="marcos">MARCOS</div>            
+        </a>    
 
-        <div class="productDiv s8" onclick="window.location.replace('productos/tintas.php')">
-            <img src="img/menu/tinta.jpg">
-            <div id="tintas">TINTAS</div>
-        </div>
+        <a class="productDiv s8" href="productos/raseros.php">            
+            <div class="imgBackground" style=" background-image: url(img/menu/rasero.jpg)"></div>
+            <div class="square" id="raseros">RASEROS</div>            
+        </a>
 
+        <a class="productDiv s4" href="productos/papel.php">        
+            <div class="imgBackground" style="background-image: url(img/menu/papel.jpg)"></div>
+            <div class="square" id="papeles">PAPELES</div>
+        </a>
+
+        <a class="productDiv s8" href="productos/tintas.php">            
+            <div class="imgBackground" style="background-image: url(img/menu/tinta.jpg)"></div>
+            <div class="square" id="tintas">TINTAS</div>            
+        </a>    
+
+        <!-- SECCION MOVIL -->
         <div class="productDiv mov" onclick="window.location.replace('productos/sellos.php')">
             <img src="img/menu/sello.jpg">
             <div id="marcos">SELLOS</div>
@@ -47,48 +51,48 @@
 
 <div class="products-content products3" >
         
-        <div class="productDiv s4" onclick="window.location.replace('productos/marcos.php')">
-            <img src="img/menu/gorra.jpg">
-            <div id="marcos">GORRAS</div>
-        </div>
+        <a href="productos/gorras.php" class="productDiv s4">
+            <div class="imgBackground" style="background-image: url(img/menu/gorra.jpg)"></div>
+            <div class="square" id="marcos">GORRAS</div>
+        </a>
 
-        <div class="productDiv s8" onclick="window.location.replace('productos/raseros.php')">
-            <img src="img/menu/tintas-y-mas.jpg">
-            <div id="raseros">TINTAS Y MAS</div>
-        </div>
+        <a href="productos/tintas-mas.php" class="productDiv s8">
+            <div class="imgBackground" style="background-image: url(img/menu/tintas-y-mas.jpg)"></div>
+            <div class="square" id="raseros">TINTAS Y MAS</div>
+        </a>
 
-        <div class="productDiv s4 menu-links"  style="background: #cacaca">
+        <div class="productDiv s4 menu-links"  style="background: #e7e7e7">
             
             
         </div>
 
-        <div class="productDiv s8" onclick="window.location.replace('productos/tintas.php')">
-            <img src="img/menu/taza.jpg">
-            <div id="tintas">TAZAS</div>
-        </div>        
+        <a href="productos/taza.php"  class="productDiv s8">
+            <div class="imgBackground" style="background-image: url(img/menu/taza.jpg)"></div>
+            <div class="square" id="tintas">TAZAS</div>
+        </a>        
         
 </div>    
             
             <!-- SEGUNDA SECCION DE PRODUCTOS -->
  
-<div class="products-content2">
+<div class="products-content2 products3">
     
-    <div class="productDiv s4 sello" onclick="window.location.replace('productos/sellos.php')">
-        <img src="img/menu/sello.jpg">
-        <div id="marcos">SELLOS</div>
-    </div>
+    <a class="productDiv s4 sello" href="productos/sellos.php">
+        <div class="imgBackground" style="background-image: url(img/menu/sello.jpg)"></div>
+        <div class="square" id="marcos">SELLOS</div>
+    </a>
 
     <div class="s8 finalProduct">
     
-        <div class="pro2 productDiv sello" onclick="window.location.replace('productos/vinil.php')">
-            <img src="img/menu/vinil.jpg">
-            <div id="raseros">VINILES</div>
-        </div>
+        <a class="pro2 productDiv sello" href="productos/vinil.php">
+            <div class="imgBackground" style="background-image: url(img/menu/vinil.jpg)"></div>
+            <div class="square" id="raseros">VINILES</div>
+        </a>
 
-        <div class=" pro2 productDiv sello" onclick="window.location.replace('productos/encuadernacion.php')">
-            <img src="img/menu/encuadernacion.jpg">
-            <div id="raseros">ENCUADERNACIÓN</div>
-        </div>
+        <a class=" pro2 productDiv sello" href="productos/encuadernacion.php">
+            <div class="imgBackground" style="background-image: url(img/menu/encuadernacion.jpg)"></div>
+            <div class="square" id="raseros">ENCUADERNACIÓN</div>
+        </a>
 
     </div>
 
